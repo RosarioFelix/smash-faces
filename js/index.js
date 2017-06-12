a23b78fff2e3b865b58262874980da5972f06adc
unknown <you@example.com>
2017-06-12 06:59:31 -0500

    subiendo casi

--- js/index.js
+++ js/index.js
@@ -1,13 +1,72 @@
 'use strict'
-$(function () {
 
-const imgLabo = (,){
-  const img
+$(function() {
+
+const ok = $('#ok').hide();
+const fail = $('#fail').hide();
+const next = $('#next').hide();
+
+let points = $('#points');
+let labo;
+let coderPais;
+let nombrePais;
+let contClick;;
+let contador = 0;
+
+function showCoders(coderPais,nombrePais) {
+        labo = coderPais[Math.round(Math.random() * coderPais.length)];
+        $('img').attr('src', 'fotos/' + nombrePais + '/' + labo.image);
+        $('img').attr('alt', "coders-Laboratoria")
+        console.log(labo);
+      }
+
+$('select').on('change', (e) => {
+  const option = $("select").val();
+  if(option == '1'){
+  showCoders(coderPais = peru , nombrePais = "peru");
+} else{
+  showCoders(coderPais = mexico ,nombrePais = "mexico");
 }
+});
 
-}
+ $('#boton').on('click',  (e) => {
 
+    contClick += 1;
 
-const root = $(.root){
-  render(root);
-}
+    let laboName = labo.name.toUpperCase();
+    const inputText = $('input').val().toUpperCase();
+    const emptyInput = $("input").val("");
+
+function interval(){ // funcion de interval
+    showCoders(coderPais,nombrePais);
+    emptyInput;
+    contClick = 0;
+};
+
+  if (inputText == laboName){
+    ok.show();
+    fail.hide(); // desaparece  el fail  de los intentos.
+    points.text(contador += 5);
+    console.log(contador);
+
+    setTimeout(function(){
+      interval();
+      ok.hide()},1800);
+
+} else if((contClick<= 5) && (inputText != laboName) && (inputText != "")){
+      fail.show()
+      emptyInput;
+      console.log(contClick)
+
+} else if ((contClick >= 5) && (inputText != laboName)){
+      fail.hide()
+      next.show()
+      points.text(contador -=1);
+
+  setTimeout(function(){
+      interval();
+      next.hide()},1000);
+  }
+
+  });
+});
